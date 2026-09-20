baa92bcea9d1410495a2e9da35e70f454bc6a8a7
mohammed-abDev <mohammedabdudev11@gmail.com>
2026-09-20 15:44:23 +0300

    Fix MySQL pool connection

--- db/dbConfige.js
+++ db/dbConfige.js
@@ -6,9 +6,6 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
     port: Number(process.env.DB_PORT || 4000),
      ssl:
     process.env.TIDB_ENABLE_SSL === "true"
